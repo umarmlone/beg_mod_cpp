51368d0a80ba403a449108a7a85f1b7590ae0e61
umarmlone <71119730+umarmlone@users.noreply.github.com>
2024-03-28 15:56:23 +0530

    Added forwarding support

--- Section 12.5/Binder/Binder/main.cpp
+++ Section 12.5/Binder/Binder/main.cpp
@@ -22,20 +22,22 @@ int main() {
 	using namespace std::placeholders ;
 	//std::function<int(int,int)> f = std::bind(Subtract,std::placeholders::_2,std::placeholders::_1) ;
 	//auto f = std::bind(Subtract,_1, 5) ;
-	auto f = [](int x) {
-		return Subtract(x,5) ;
+	auto f = [](auto && x) {
+		return Subtract(std::forward<decltype(x)>(x),5) ;
 	} ;
 	std::cout << f(8) << '\n' ; //Subtract(2,3) ;
 
 	//auto gt = std::bind(GreaterThan{}, _2, _1) ;
-	auto gt = [](int x,int y) {
-		return GreaterThan{}(y, x) ;
+	//C++20 template type parameters in lambda
+	auto gt = []<typename T>(T&& x,T&& y) {
+		return GreaterThan{}(std::forward<T>(y), std::forward<T>(x)) ;
 	} ;
 	std::cout << gt(6,2) << '\n' ; //operator()(2,6)
 
 	//auto acc = std::bind(Accumulate,_1, _2, 0) ;
-	auto acc = [](int x,int y) {
-		return Accumulate(x,y, 0) ;
+	//C++20 template type parameters in lambda
+	auto acc = []<typename T>(T&& x,T&& y) {
+		return Accumulate(std::forward<T>(x),std::forward<T>(y), 0) ;
 	} ;
 	std::cout << Operation(8,5, acc) << '\n';
 
